0107d003165e5e2b93314e8e295e4c9a20757c9c
Xingyu Li <lll109512@outlook.com>
2021-08-16 13:26:19 +0800

    save

--- src/components/LongPressUnlockSlider/Cursor.tsx
+++ src/components/LongPressUnlockSlider/Cursor.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Easing, StyleSheet, Text, View } from 'react-native'
-import { PanGestureHandler, PanGestureHandlerGestureEvent } from 'react-native-gesture-handler'
-import Animated,{runOnJS, useAnimatedGestureHandler, useAnimatedStyle, useDerivedValue, useSharedValue, withTiming} from 'react-native-reanimated'
+import { LongPressGestureHandler, PanGestureHandler, PanGestureHandlerGestureEvent, State, TapGestureHandler } from 'react-native-gesture-handler'
+import Animated,{runOnJS, useAnimatedGestureHandler, useAnimatedStyle, useDerivedValue, useSharedValue, withTiming,useAnimatedProps} from 'react-native-reanimated'
 import {CIRCLE,PADDING} from './constants.js'
 import AntDesign from 'react-native-vector-icons/AntDesign'
+import { useRef } from 'react'
 
+// const AnimatedAntDesign = Animated.createAnimatedComponent(AntDesign)
 interface Props {
     iconName: string,
     translateX: Animated.SharedValue<number>,
@@ -16,17 +18,23 @@ interface Props {
 }
 
 const Cursor = (props: Props) => {
+    // const [cursorActived, setCursorActived] = useState<boolean>(false)
     const rightSideMaxX = props.maxWidth - CIRCLE - PADDING * 2
     const contextX = useSharedValue<number>(0)
+    const cursorActived = useSharedValue<boolean>(false)
     const onGestureHandler = useAnimatedGestureHandler<PanGestureHandlerGestureEvent>({
         onStart:()=>{
             contextX.value = props.translateX.value
-            runOnJS(props.setIsDraging)(true)
+            // if(cursorActived.value){
+            //     runOnJS(props.setIsDraging)(true)
+            // }
         },
         onActive:({translationX,x})=>{
-            props.translateX.value = translationX + contextX.value
+            if(cursorActived.value){
+                props.translateX.value = translationX + contextX.value
+            }
         },
-        onEnd:({velocityX,translationX})=>{
+        onEnd:()=>{
             if(props.translateX.value >= rightSideMaxX * props.threshHold){
                 props.translateX.value = withTiming(rightSideMaxX)
                 runOnJS(props.onOpen)()
@@ -35,9 +43,9 @@ const Cursor = (props: Props) => {
                 runOnJS(props.onClose)()
             }
         },
-        onFinish:()=>{
-            runOnJS(props.setIsDraging)(false)
-        }
+        // onFinish:()=>{
+        //     runOnJS(props.setIsDraging)(false)
+        // }
     })
     const inRangeX = useDerivedValue(()=>{
         return Math.min(Math.max(props.translateX.value,0),rightSideMaxX)
@@ -47,12 +55,33 @@ const Cursor = (props: Props) => {
             transform:[{translateX:inRangeX.value}]
         }
     })
+    const longPressHandlerRef = useRef(null)
+    const panHandlerRef = useRef(null)
     return (
-         <PanGestureHandler onGestureEvent={onGestureHandler}>
-            <Animated.View style={[styles.root,style]}>
-                <Text>Drag</Text>
-            </Animated.View>
-         </PanGestureHandler>
+        <LongPressGestureHandler 
+            ref={longPressHandlerRef}
+            simultaneousHandlers={panHandlerRef}
+            minDurationMs={500} 
+            onHandlerStateChange={({ nativeEvent, }) => {
+                if (nativeEvent.state === State.ACTIVE) {
+                    console.log('long pressed')
+                    cursorActived.value = true
+                }
+                if (nativeEvent.state === State.END) {
+                    console.log('long pressed end')
+                    cursorActived.value = false
+                }
+            }
+        }>
+            <View>
+                <PanGestureHandler onGestureEvent={onGestureHandler} ref={panHandlerRef}>
+                    <Animated.View style={[styles.root,style]}>
+                        {/* <Text>Drag</Text> */}
+                        <AntDesign name={props.iconName} size={30}/>
+                    </Animated.View>
+                </PanGestureHandler>
+            </View>
+        </LongPressGestureHandler>
     )
 }
 
--- src/components/LongPressUnlockSlider/index.tsx
+++ src/components/LongPressUnlockSlider/index.tsx
@@ -1,6 +1,7 @@
 import React,{useState} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import Animated, { useSharedValue } from 'react-native-reanimated'
+import Animated, { interpolate, useAnimatedStyle, useDerivedValue, useSharedValue,interpolateColors } from 'react-native-reanimated'
+import { mixColor } from 'react-native-redash'
 import {HEIGHT,CIRCLE,UNLOCK_COLOR,LOCKED_COLOR,PADDING} from './constants.js'
 import Cursor from './Cursor'
 
@@ -20,15 +21,27 @@ const index = (props: Props) => {
     const onClose = ()=>{
        setOpen(false) 
     }
+
+    const backgroundColor = useDerivedValue(() => {
+        const normValue = interpolate(translateX.value,[0,maxWidth - CIRCLE - PADDING*2],[0,1])
+        return mixColor(normValue, LOCKED_COLOR, UNLOCK_COLOR)
+    });
+    
+    const astyle = useAnimatedStyle(()=>{
+        return {
+            backgroundColor:backgroundColor.value,
+        }
+    })
+
     return (
         <View>
-            <Animated.View style={[styles.bar,{backgroundColor:LOCKED_COLOR}]} onLayout={(e)=>setMaxWidth(e.nativeEvent.layout.width)}>
+            <Animated.View style={[styles.bar,astyle]} onLayout={(e)=>setMaxWidth(e.nativeEvent.layout.width)}>
                 <View style={[styles.backTextView,StyleSheet.absoluteFillObject]}>
                     {!isDraging &&
                     <Text style={styles.backText}>{open?'Unlocked':'Locked'}</Text>
                     }
                 </View>
-                <Cursor maxWidth={maxWidth} translateX={translateX} iconName={'123'} setIsDraging={setIsDraging} threshHold={0.8} onOpen={onOpen} onClose={onClose}/>
+                <Cursor maxWidth={maxWidth} translateX={translateX} iconName={open?'unlock':'lock1'} setIsDraging={setIsDraging} threshHold={0.8} onOpen={onOpen} onClose={onClose}/>
             </Animated.View>
         </View>
     )
