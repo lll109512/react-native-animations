83d3ad60ebd1c9450e79e7d54bc6c58ab99495ab
Xingyu Li <lll109512@outlook.com>
2021-08-18 10:09:57 +0800

    finish long press slider

--- src/components/LongPressUnlockSlider/Cursor.tsx
+++ src/components/LongPressUnlockSlider/Cursor.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import { Easing, StyleSheet, Text, View } from 'react-native'
 import { LongPressGestureHandler, PanGestureHandler, PanGestureHandlerGestureEvent, State, TapGestureHandler } from 'react-native-gesture-handler'
-import Animated,{runOnJS, useAnimatedGestureHandler, useAnimatedStyle, useDerivedValue, useSharedValue, withTiming,useAnimatedProps} from 'react-native-reanimated'
+import Animated,{runOnJS, useAnimatedGestureHandler, useAnimatedStyle, useDerivedValue, useSharedValue, withTiming,useAnimatedProps, interpolate, Extrapolate} from 'react-native-reanimated'
 import {CIRCLE,PADDING} from './constants.js'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 import { useRef } from 'react'
+import { mixColor } from 'react-native-redash'
 
 // const AnimatedAntDesign = Animated.createAnimatedComponent(AntDesign)
 interface Props {
@@ -22,6 +23,8 @@ const Cursor = (props: Props) => {
     const rightSideMaxX = props.maxWidth - CIRCLE - PADDING * 2
     const contextX = useSharedValue<number>(0)
     const cursorActived = useSharedValue<boolean>(false)
+    const pressedDuration = useSharedValue<number>(0)
+    let timer:NodeJS.Timer
     const onGestureHandler = useAnimatedGestureHandler<PanGestureHandlerGestureEvent>({
         onStart:()=>{
             contextX.value = props.translateX.value
@@ -42,6 +45,7 @@ const Cursor = (props: Props) => {
                 props.translateX.value = withTiming(0)
                 runOnJS(props.onClose)()
             }
+             cursorActived.value = false
         },
         // onFinish:()=>{
         //     runOnJS(props.setIsDraging)(false)
@@ -51,29 +55,39 @@ const Cursor = (props: Props) => {
         return Math.min(Math.max(props.translateX.value,0),rightSideMaxX)
     })
     const style = useAnimatedStyle(()=>{
+        const normValue = interpolate(pressedDuration.value,[0,1000],[0,1],Extrapolate.CLAMP)
+        const color = mixColor(normValue, '#fff', '#24b322')
         return {
-            transform:[{translateX:inRangeX.value}]
+            transform:[{translateX:inRangeX.value}],
+            borderColor:color
         }
     })
     const longPressHandlerRef = useRef(null)
     const panHandlerRef = useRef(null)
+
     return (
-        <LongPressGestureHandler 
+        <TapGestureHandler 
             ref={longPressHandlerRef}
             simultaneousHandlers={panHandlerRef}
-            minDurationMs={500} 
             onHandlerStateChange={({ nativeEvent, }) => {
-                if (nativeEvent.state === State.ACTIVE) {
-                    console.log('long pressed')
-                    cursorActived.value = true
+                if (nativeEvent.state === State.BEGAN) {
+                    console.log('long pressed begined')
+                    timer = setInterval(()=>{
+                        if(pressedDuration.value >= 1000){
+                            clearInterval(timer)
+                            cursorActived.value = true
+                        }
+                        pressedDuration.value = withTiming(500 + pressedDuration.value,{duration:500})
+                    },500)
                 }
                 if (nativeEvent.state === State.END) {
                     console.log('long pressed end')
-                    cursorActived.value = false
+                    clearInterval(timer)
+                    pressedDuration.value = withTiming(0)
                 }
             }
         }>
-            <View>
+            <View style={{width:CIRCLE}}>
                 <PanGestureHandler onGestureEvent={onGestureHandler} ref={panHandlerRef}>
                     <Animated.View style={[styles.root,style]}>
                         {/* <Text>Drag</Text> */}
@@ -81,7 +95,7 @@ const Cursor = (props: Props) => {
                     </Animated.View>
                 </PanGestureHandler>
             </View>
-        </LongPressGestureHandler>
+        </TapGestureHandler>
     )
 }
 
@@ -97,6 +111,8 @@ const styles = StyleSheet.create({
         // position:"absolute",
         alignItems:'center',
         justifyContent:"center",
+        borderColor:"#fff",
+        borderWidth:3,
         // left:0,
     }
 })
