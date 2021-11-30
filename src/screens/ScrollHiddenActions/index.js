8f7d3b0b27e882822e2a558c250cf06e033c0670
Xingyu Li <lll109512@outlook.com>
2021-11-30 10:11:44 +0800

    save

--- src/screens/ScrollHiddenActions/index.js
+++ src/screens/ScrollHiddenActions/index.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import faker from "faker";
 import { BlurView } from "@react-native-community/blur";
-import Animated from 'react-native-reanimated'
+import Animated, { useAnimatedScrollHandler, useAnimatedStyle, useDerivedValue, useSharedValue, withTiming } from 'react-native-reanimated'
 import { StyleSheet, Text, View,Dimensions } from 'react-native';
 const screen = Dimensions.get('screen')
 const BlurHeader = (props)=>{
-    const {title} = props
+    const { title, animatedIndex } = props;
+    const rStyle = useAnimatedStyle(() => {
+        return { transform: [{ translateY: -animatedIndex.value * 40 }] };
+    });
     return (
-        <View style={styles.header}>
+        <Animated.View style={[styles.header, rStyle]}>
             <BlurView blurType="xlight" blurAmount={10} reducedTransparencyFallbackColor="white">
                 <Text style={styles.headerText}>{title}</Text>
             </BlurView>
-        </View>
+        </Animated.View>
     );
 }
 
 const BlurActions = (props)=>{
-    const {title} = props
+    const { animatedIndex } = props;
+    const rStyle = useAnimatedStyle(()=>{
+        return { transform: [{ translateY: animatedIndex.value * 80 }] };
+    })
     return (
-        <View style={styles.actions}>
+        <Animated.View style={[styles.actions, rStyle]}>
             <View style={styles.container}>
                 <BlurView
                     blurType="xlight"
@@ -30,18 +36,50 @@ const BlurActions = (props)=>{
                     </View>
                 </BlurView>
             </View>
-        </View>
+        </Animated.View>
     );
 }
 const index = (props) => {
     const data = Array(20).fill(0).map(_ => faker.lorem.paragraph());
-    return <View style={styles.root}>
-        <Animated.ScrollView style={{paddingTop:90}}>
-            {data.map(item=>(<Text style={styles.paragraph}>{item}</Text>))}
-        </Animated.ScrollView>
-        <BlurHeader title='Article'/>
-        <BlurActions/>
-    </View>;
+    const scrollY = useSharedValue(0) 
+    const revertPointY = useSharedValue(0)
+    const previousY = useSharedValue(0)
+    // const actionsIndex = useSharedValue(0)
+    // const direction = useDerivedValue(()=>{
+    //     if(scrollY.value > previousY.value){
+    //         return 1
+    //     }else{
+    //         return 0
+    //     }
+    // })
+    const actionsIndex = useDerivedValue(() => {
+        if (scrollY.value > previousY.value) {
+            return withTiming(1,{duration:500})
+        } else {
+            return withTiming(0,{duration:500})
+        }
+    });
+    const onScroll = useAnimatedScrollHandler({
+        onScroll: ({ contentOffset: { y } }) => {
+            previousY.value = scrollY.value
+            scrollY.value = y;
+        },
+    });
+    return (
+        <View style={styles.root}>
+            <Animated.ScrollView
+                style={{ paddingTop: 90 }}
+                onScroll={onScroll}
+                scrollEventThrottle={16}
+            >
+                {data.map(item => (
+                    <Text style={styles.paragraph}>{item}</Text>
+                ))}
+            </Animated.ScrollView>
+            <BlurHeader title="Article" animatedIndex={actionsIndex} />
+            <BlurActions animatedIndex={actionsIndex} />
+        </View>
+    );
 }
 
 export default index
@@ -76,7 +114,7 @@ const styles = StyleSheet.create({
         flexDirection: "row",
     },
     actionsBar: {
-        height: 100,
+        height: 80,
         width: screen.width,
         borderRadius: 10,
         // borderWidth: 0.2,
